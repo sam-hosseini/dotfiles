009f8fecad3536d4c33e6ea93d8926ed126c6c78
Sam Hosseini <non-existent-email@hosseini.io>
2017-12-02 00:38:56 +0200

    remove PATH updates of Postgres and Compass

--- fish_shell/config.fish
+++ fish_shell/config.fish
@@ -1,11 +1,6 @@
 # Remove fish_greeting
 set fish_greeting
 
-# Update PATH
-set --local POSTGRES_PATH /Applications/Postgres.app/Contents/Versions/latest/bin
-set --local RUBY_PATH /Users/shos/.gem/ruby/2.0.0/bin
-set PATH $PATH $POSTGRES_PATH $RUBY_PATH
-
 # Set locale
 set --global --export LC_ALL en_US.UTF-8
 set --global --export LANG en_US.UTF-8
