df1a5fb378470507184c8af306c31cc476314b6b
Sam Hosseini <non-existent-email@hosseini.io>
2017-12-25 12:15:37 +0100

    add a bash script to compile the compiled component of YouCompleteMe plugin of vim

---
+++ vim/compile-YouCompleteMe.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Change working directory to YouCompleteMe plugin folder
+cd ~/.vim/bundle/YouCompleteMe
+
+# Compiling the compiled component of YouCompleteMe
+python3 install.py
