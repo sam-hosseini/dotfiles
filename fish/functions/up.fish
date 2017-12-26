9cab8213494c4cc01634f091617e206759a7bf16
Sam Hosseini <non-existent-email@hosseini.io>
2017-12-26 01:56:16 +0100

    add omf install and update to automatically install packages within bundle and update them to up function of Fish shell

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,4 +1,5 @@
 function up
     brew update; brew upgrade; brew prune; brew cleanup; brew doctor;
     pip3 install --upgrade pip setuptools wheel
+    omf install; omf update; omf reload; omf doctor;
 end
