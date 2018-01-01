48a41b2f90be153e4abfee7878dc3ea1cc8964d8
Sam Hosseini <non-existent-email@hosseini.io>
2018-01-01 17:18:03 +0100

    fix the mistake of setting fish_user_path with --universal flag everytime a new shell was run, causing the shell to become slow, 80-charachter rule fixes

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set fish_greeting
 
 ####### Environment Variables #######
 # Ensure homebrew programs will be used before system-provided programs
-set --universal fish_user_paths /usr/local/bin $fish_user_paths
+set --global --export fish_user_paths /usr/local/bin $fish_user_paths
 
 # Disabling bytecode (.pyc) files
 set --global --export PYTHONDONTWRITEBYTECODE 1
@@ -16,14 +16,16 @@ set --global --export LANG en_US.UTF-8
 set --global --export EDITOR vim
 
 # Configuration for Powerline
-set --global --export POWERLINE_ROOT_REPO /usr/local/lib/python2.7/site-packages
-set fish_function_path $fish_function_path "$POWERLINE_ROOT_REPO/powerline/bindings/fish"
+set --global --export \
+    POWERLINE_ROOT_REPO /usr/local/lib/python2.7/site-packages
+set fish_function_path \
+    $fish_function_path "$POWERLINE_ROOT_REPO/powerline/bindings/fish"
 powerline-daemon --quiet
 powerline-setup
 
 # Configuration for Oh-My-Fish!
 set -q XDG_DATA_HOME
-    and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-    or set -gx OMF_PATH "$HOME/.local/share/omf"
+    and set --global --export OMF_PATH "$XDG_DATA_HOME/omf"
+    or set  --global --export OMF_PATH "$HOME/.local/share/omf"
 source $OMF_PATH/init.fish
 
