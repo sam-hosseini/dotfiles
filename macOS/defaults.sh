b74dd53efef8037c34d000df2a471ce0351271f9
Sam Hosseini <non-existent-email@hosseini.io>
2018-01-02 21:02:56 +0100

    add finder configurations to defaults.sh

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -4,14 +4,13 @@ declare -a AFFECTED_APPS=(
     "Amphetamine" \
     "GIPHY CAPTURE" \
     "Transmission" \
+    "Finder" \
 )
 
 main() {
-    # Quit all apps we're going to configure                                      #
     quit_all_affected_apps
-    # Configure all apps whose configurations are with plists
-    configure_plist_apps
-    # Start all apps we configured
+    configure_plist_apps # Configure all apps whose configurations are plists
+    configure_finder
     start_all_affected_apps
 }
 
@@ -26,6 +25,39 @@ function configure_plist_apps() {
     import_plist "com.if.Amphetamine" "Amphetamine.plist"
     import_plist "com.fasthatchapps.gifgrabberosx" "GIPHY_Capture.plist"
     import_plist "org.m0k.transmission" "Transmission.plist"
+}
+
+function configure_finder() {
+    # allow quitting via ⌘ + q; doing so will also hide desktop icons
+    defaults write com.apple.finder QuitMenuItem -bool true
+    # disable window animations and Get Info animations
+    defaults write com.apple.finder DisableAllAnimations -bool true
+    # Set Downloads as the default location for new Finder windows
+    defaults write com.apple.finder NewWindowTarget -string "PfLo"
+    defaults write com.apple.finder NewWindowTargetPath -string \
+        "file://${HOME}/Downloads/"
+    # disable status bar
+    defaults write com.apple.finder ShowStatusBar -bool false
+    # disable path bar
+    defaults write com.apple.finder ShowPathbar -bool false
+    # Display full POSIX path as Finder window title
+    defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+    # Keep folders on top when sorting by name
+    defaults write com.apple.finder _FXSortFoldersFirst -bool true
+    # When performing a search, search the current folder by default
+    defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+    # Disable disk image verification
+    defaults write com.apple.frameworks.diskimages \
+        skip-verify -bool true
+    defaults write com.apple.frameworks.diskimages \
+        skip-verify-locked -bool true
+    defaults write com.apple.frameworks.diskimages \
+        skip-verify-remote -bool true
+    # Use list view in all Finder windows by default
+    # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+    defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+    # Disable the warning before emptying the Trash
+    defaults write com.apple.finder WarnOnEmptyTrash -bool false
 }
 
 function start_all_affected_apps() {
