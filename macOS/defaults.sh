1d4b04d422bdd3e5be22c3efe7b45f46f8f603c7
Sam Hosseini <non-existent-email@hosseini.io>
2018-01-02 20:29:53 +0100

    refactor defaults.sh into a function based top-to-bottom approach

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -1,14 +1,42 @@
 #!/usr/bin/env bash
 
-declare -a Apps=(
+declare -a AFFECTED_APPS=(
     "Amphetamine" \
     "GIPHY CAPTURE" \
     "Transmission" \
 )
 
-###############################################################################
-# Helper functions                                                            #
-###############################################################################
+main() {
+    # Quit all apps we're going to configure                                      #
+    quit_all_affected_apps
+    # Configure all apps whose configurations are with plists
+    configure_plist_apps
+    # Start all apps we configured
+    start_all_affected_apps
+}
+
+function quit_all_affected_apps() {
+    for app in "${AFFECTED_APPS[@]}"
+    do
+        killall "$app" > /dev/null 2>&1
+    done
+}
+
+function configure_plist_apps() {
+    import_plist "com.if.Amphetamine" "Amphetamine.plist"
+    import_plist "com.fasthatchapps.gifgrabberosx" "GIPHY_Capture.plist"
+    import_plist "org.m0k.transmission" "Transmission.plist"
+}
+
+function start_all_affected_apps() {
+    for app in "${AFFECTED_APPS[@]}"
+    do
+        osascript << EOM
+tell application "$app" to activate
+EOM
+    done
+}
+
 function import_plist() {
     domain=$1
     filename=$2
@@ -16,27 +44,4 @@ function import_plist() {
     defaults import "$domain" "$filename"
 }
 
-###############################################################################
-# Quit all apps we're going to configure                                      #
-###############################################################################
-for app in "${Apps[@]}"
-do
-    killall "$app" > /dev/null 2>&1
-done
-
-###############################################################################
-# Apps to configure using plist                                               #
-###############################################################################
-import_plist "com.if.Amphetamine" "Amphetamine.plist"
-import_plist "com.fasthatchapps.gifgrabberosx" "GIPHY_Capture.plist"
-import_plist "org.m0k.transmission" "Transmission.plist"
-
-###############################################################################
-# Start all apps we configured                                                #
-###############################################################################
-for app in "${Apps[@]}"
-do
-    osascript << EOM
-tell application "$app" to activate
-EOM
-done
+main "$@"
