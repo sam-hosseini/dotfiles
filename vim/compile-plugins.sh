1bc3672c2980ac39de2d1550f3d4347629f8ef8f
Sam Hosseini <non-existent-email@hosseini.io>
2018-01-02 19:45:14 +0100

    add compilation step of command-t vim plugin C extention

--- vim/compile-plugins.sh
+++ vim/compile-plugins.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR=$(pwd)
+
 # Compiling the compiled component of YouCompleteMe
 python3 ~/.vim/bundle/YouCompleteMe/install.py
+
+# Compiling the compiled component of Command-t
+cd ~/.vim/bundle/command-t
+rake make
+cd $CURRENT_DIR
