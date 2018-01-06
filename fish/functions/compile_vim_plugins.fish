77c63f423dba9cb63fb8f1d601b292e366119c38
Sam Hosseini <non-existent-email@hosseini.io>
2018-01-06 01:14:12 +0100

    add compile_vim_plugins fish shell function instead of having a separate bash script to compile the vim plugins

---
+++ fish/functions/compile_vim_plugins.fish
@@ -0,0 +1,11 @@
+function compile_vim_plugins
+    set --local CURRENT_DIR (pwd)
+
+    # Compiling the compiled component of YouCompleteMe
+    python3 ~/.vim/bundle/YouCompleteMe/install.py
+
+    # Compiling the compiled component of Command-t
+    cd ~/.vim/bundle/command-t
+    rake make
+    cd $CURRENT_DIR
+end
