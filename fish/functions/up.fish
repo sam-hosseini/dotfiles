8b304c2045a4de27d042e59dbc7415c243f51454
Sam Hosseini <non-existent-email@hosseini.io>
2018-01-29 19:49:21 +0200

    add upgrading of casks in homebrew to up function of fish shell

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,6 +1,6 @@
 function up
     sudo softwareupdate --install --all
-    brew update; brew upgrade; brew prune; brew cleanup; brew doctor;
+    brew update; brew upgrade; brew cask upgrade; brew prune; brew cleanup; brew doctor;
     pip3 install --upgrade pip setuptools wheel
     omf install; omf update; omf reload; omf doctor;
 end
