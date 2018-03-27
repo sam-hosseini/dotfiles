970bea0cc64dbb0b690f2b22765584471aed6996
Sam Hosseini <non-existent-email@hosseini.io>
2018-03-27 15:45:46 +0300

    update the momentum_wallpaper.fish function to delete the old momentum pictures now that every new picture has a randomzied name inside the dropbox pictures folder

--- fish/functions/momentum_wallpaper.fish
+++ fish/functions/momentum_wallpaper.fish
@@ -1,6 +1,10 @@
 function momentum_wallpaper
     set --local BASE_DIR ~/personal/dotfiles/macOS
-    python3 $BASE_DIR/momentum_wallpaper.py $argv
-    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$BASE_DIR/todays_picture.jpg\""
+    set --local DROPBOX_IMAGES ~/Dropbox/Pictures/todays_picture_*.jpg
+    set --local PYTHON_SCRIPT $BASE_DIR/momentum_wallpaper.py
+    set --local WALLPAPER $BASE_DIR/todays_picture.jpg
+    rm -f $DROPBOX_IMAGES
+    python3 $PYTHON_SCRIPT $argv
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
     killall Dock
 end
