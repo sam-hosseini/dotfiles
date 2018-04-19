62401b295a42015aecaf1ac481cc86ab3252e827
Sam Hosseini <non-existent-email@hosseini.io>
2018-04-19 15:02:29 +0300

    remove top right and bottom left hot corners

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -73,10 +73,10 @@ function configure_dock() {
     defaults write com.apple.dock wvous-tl-corner -int 2
     defaults write com.apple.dock wvous-tl-modifier -int 0
     ## Top right screen corner → Desktop
-    defaults write com.apple.dock wvous-tr-corner -int 4
+    defaults write com.apple.dock wvous-tr-corner -int 0
     defaults write com.apple.dock wvous-tr-modifier -int 0
     ## Bottom left screen corner → Start screen saver
-    defaults write com.apple.dock wvous-bl-corner -int 5
+    defaults write com.apple.dock wvous-bl-corner -int 0
     defaults write com.apple.dock wvous-bl-modifier -int 0
     open "Dock"
 }
