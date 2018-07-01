ff56e5efe47e7496c50f94c1f0a3b35b28c7abfb
Sam Hosseini <non-existent-email@hosseini.io>
2018-07-01 20:36:22 +0300

    change the python exectutor of installation script of YouCompleteMe from python3 to python. this python is homebrew's python since that has been just added in the previous commit to the macOS.Brewfile

--- fish/functions/compile_vim_plugins.fish
+++ fish/functions/compile_vim_plugins.fish
@@ -2,7 +2,7 @@ function compile_vim_plugins
     set --local CURRENT_DIR (pwd)
 
     # Compiling the compiled component of YouCompleteMe
-    python3 ~/.vim/bundle/YouCompleteMe/install.py
+    python ~/.vim/bundle/YouCompleteMe/install.py
 
     # Compiling the compiled component of Command-t
     cd ~/.vim/bundle/command-t
