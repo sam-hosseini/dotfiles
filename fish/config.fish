eac89ba590de41285e2ae7db200943d873922216
Sam Hosseini <non-existent-email@hosseini.io>
2018-09-24 16:31:07 +0300

    cleanup fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,18 @@
-# Remove fish_greeting
-set fish_greeting
-
-####### Environment Variables #######
-# Disable brew auto update
+############################################################################
+# Environment Variables
+############################################################################
 set --global --export HOMEBREW_NO_AUTO_UPDATE true
-
-# Ensure homebrew programs will be used before system-provided programs
-set --global --export fish_user_paths /usr/local/bin $fish_user_paths
-
-# Disabling bytecode (.pyc) files
-set --global --export PYTHONDONTWRITEBYTECODE 1
-
-# Set locale
-set --global --export LC_ALL en_US.UTF-8
-set --global --export LANG en_US.UTF-8
-
-# Set EDITOR
 set --global --export EDITOR vim
+set --global --export PYTHONDONTWRITEBYTECODE 1
+set --global --export fish_greeting ''
+set --global --export LC_ALL en_US.UTF-8 # Set locale
+set --global --export LANG en_US.UTF-8   # Set locale
+set --global --export fish_user_paths /usr/local/bin $fish_user_paths # Homebrew programs will be used before system-provided programs
 
+
+############################################################################
 # Configuration for Powerline
+############################################################################
 set --global --export \
     POWERLINE_ROOT_REPO /usr/local/lib/python3.6/site-packages
 set fish_function_path \
@@ -26,7 +20,9 @@ set fish_function_path \
 powerline-daemon --quiet
 powerline-setup
 
+############################################################################
 # Configuration for Oh-My-Fish!
+############################################################################
 set -q XDG_DATA_HOME
     and set --global --export OMF_PATH "$XDG_DATA_HOME/omf"
     or set  --global --export OMF_PATH "$HOME/.local/share/omf"
