f247524e540fa8a75356261ac010cb268a539632
Sam Hosseini <non-existent-email@hosseini.io>
2018-11-18 16:20:28 +0200

    add yarn global update to fish up function so that my global yarn packages would be updated as well

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -2,5 +2,6 @@ function up
     sudo softwareupdate --install --all
     brew update; brew upgrade; brew cask upgrade; brew prune; brew cleanup; brew doctor;
     pip3 install --upgrade pip setuptools wheel
+    yarn global upgrade
     omf install; omf update; omf reload; omf doctor;
 end
