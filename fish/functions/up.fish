afeeda7237a119581c65923c48321991d112a59b
Sam Hosseini <non-existent-email@hosseini.io>
2018-12-04 22:06:01 +0200

    remove dotenv package installation and upgrade from setup-mac-os.sh and up fish shell function

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,7 +1,7 @@
 function up
     sudo softwareupdate --install --all
     brew update; brew upgrade; brew cask upgrade; brew prune; brew cleanup; brew doctor;
-    pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests python-dotenv flake8
+    pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests flake8
     yarn global upgrade --silent
     omf install; omf update; omf reload; omf doctor;
 end
