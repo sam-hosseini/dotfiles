ed895bac7f00af6aabbf04e7e6efab6ad23c8194
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-09 20:26:16 +0200

    remove brune prune from up fish shell function now that it's deprecated

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,6 +1,6 @@
 function up
     sudo softwareupdate --install --all
-    brew update; brew upgrade; brew cask upgrade; brew prune; brew cleanup; brew doctor;
+    brew update; brew upgrade; brew cask upgrade; brew cleanup; brew doctor;
     pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests flake8
     yarn global upgrade --silent
     omf install; omf update; omf doctor;
