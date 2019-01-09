e4b3cf4a47de19e5e9d919f031cf5c4bd76d3a33
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-09 21:26:32 +0200

    remove omf doctor as it basically does nothing except printing system info

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -3,5 +3,5 @@ function up
     brew update; brew upgrade; brew cask upgrade; brew cleanup; brew doctor;
     pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests flake8
     yarn global upgrade --silent
-    omf install; omf update; omf doctor;
+    omf install; omf update;
 end
