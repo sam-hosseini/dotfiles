c103545de0e708ce322ef7c709efb961489c986c
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-10 12:24:15 +0200

    change momentum_wallpaper.fish dramatically to do everything in the shell while adding the possibility of setting first and second picture of the day

--- fish/functions/momentum_wallpaper.fish
+++ fish/functions/momentum_wallpaper.fish
@@ -1,10 +1,40 @@
 function momentum_wallpaper
-    set --local BASE_DIR ~/personal/dotfiles/macOS
+
+    set --local WALLPAPER ~/personal/dotfiles/macOS/todays_picture.jpg
     set --local DROPBOX_IMAGES ~/Dropbox/Uploads/todays_picture_*.jpg
-    set --local PYTHON_SCRIPT $BASE_DIR/momentum_wallpaper.py
-    set --local WALLPAPER $BASE_DIR/todays_picture.jpg
-    rm -f $DROPBOX_IMAGES
-    python3 $PYTHON_SCRIPT $argv
+    set --local DROPBOX_PATH ~/Dropbox/Uploads/todays_picture_(random).jpg
+
+    set --local BASE_URL "https://api.momentumdash.com"
+    set --local AUTH_URL "$BASE_URL/user/authenticate"
+    set --local AUTH_RESPONSE (http --body POST $AUTH_URL username=$MOMENTUM_USERNAME password=$MOMENTUM_PASSWORD)
+    set --local AUTH_TOKEN (echo $AUTH_RESPONSE | jq --raw-output '.token')
+    set --local TODAY (date -u +"%Y-%m-%d")
+    set --local API_IMAGE_URL "$BASE_URL/feed/bulk?syncTypes=backgrounds&localDate=$TODAY"
+    set --local MOMENTUM_RESPONSE (http $API_IMAGE_URL "Authorization: Bearer $AUTH_TOKEN")
+    set --local FIRST_IMAGE_URL (echo $MOMENTUM_RESPONSE | jq --raw-output '.backgrounds[0].filename')
+    set --local SECOND_IMAGE_URL (echo $MOMENTUM_RESPONSE | jq --raw-output '.backgrounds[1].filename')
+    set --local IMAGE_URL ""
+
+    switch "$argv[1]"
+    case first
+        set IMAGE_URL $FIRST_IMAGE_URL
+    case second
+        set IMAGE_URL $SECOND_IMAGE_URL
+    case '*'
+        set IMAGE_URL $FIRST_IMAGE_URL
+    end
+
+    ############################################################################
+    # Downloading Momentum picture of the day and setting it as wallpaper
+    ############################################################################
+    wget --quiet --output-document=$WALLPAPER $IMAGE_URL
     osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
     killall Dock
+
+    ############################################################################
+    # Copying picture to Dropbox so that IFTTT can download it to iOS pictures
+    ############################################################################
+    rm -f $DROPBOX_IMAGES
+    cp $WALLPAPER $DROPBOX_PATH
+
 end
