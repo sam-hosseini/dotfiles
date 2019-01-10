c125fb15c04b1d5450861d2e83c08f52f6d98a61
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-10 12:18:03 +0200

    add bing_wallpaper fish shell function to download bing's picture of the day and set it as wallpaper

---
+++ fish/functions/bing_wallpaper.fish
@@ -0,0 +1,25 @@
+function bing_wallpaper
+
+    set --local WALLPAPER ~/personal/dotfiles/macOS/todays_picture.jpg
+    set --local DROPBOX_IMAGES ~/Dropbox/Uploads/todays_picture_*.jpg
+    set --local DROPBOX_PATH ~/Dropbox/Uploads/todays_picture_(random).jpg
+
+    set --local BASE_URL "http://www.bing.com"
+    set --local API_URL "$BASE_URL/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
+    set --local IMAGE_PATH (http --body $API_URL | jq --raw-output '.images[0].url')
+    set --local IMAGE_URL $BASE_URL$IMAGE_PATH
+
+    ############################################################################
+    # Downloading Bing's picture of today and setting it as wallpaper
+    ############################################################################
+    wget --quiet --output-document=$WALLPAPER $IMAGE_URL
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
+    killall Dock
+
+    ############################################################################
+    # Copying picture to Dropbox so that IFTTT can download it to iOS pictures
+    ############################################################################
+    rm -f $DROPBOX_IMAGES
+    cp $WALLPAPER $DROPBOX_PATH
+
+end
