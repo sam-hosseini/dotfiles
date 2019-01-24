60f2fb60a1091512a09033240d17b7a7fac59bd0
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-24 20:58:32 +0200

    add login_hook_script.sh and logout_hook_script.sh to be able to disable macOS startup chime sound

---
+++ macOS/login_hook_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+############################################################################
+# REQUIRED for disabling chime startup sound => Un-muting upon login
+############################################################################
+osascript -e 'set volume without output muted'
---
+++ macOS/logout_hook_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+############################################################################
+# REQUIRED for disabling chime startup sound => Muting upon logout
+############################################################################
+osascript -e 'set volume with output muted'
