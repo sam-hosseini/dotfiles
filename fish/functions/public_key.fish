ac6a502c47bf8a6237d36a9d841b740159d25486
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-24 19:30:56 +0200

    update public_key fish shell function to be able to handle if there's no public key file and generate one. update the copying to clipboard to trim the new line at the end as well

--- fish/functions/public_key.fish
+++ fish/functions/public_key.fish
@@ -1,3 +1,9 @@
 function public_key
-    cat ~/.ssh/id_rsa.pub | pbcopy
+    set --local PUBLIC_KEY_PATH ~/.ssh/id_rsa.pub
+
+    if test -e $PUBLIC_KEY_PATH
+        cat $PUBLIC_KEY_PATH | tr -d '\n' | pbcopy
+    else
+        ssh-keygen -f ~/.ssh/id_rsa
+    end
 end
