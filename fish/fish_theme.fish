d89c8c7337e33d22f5b59c8ddbbff07b79fb5939
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-25 01:25:40 +0200

    add fish_theme.fish which contains all the necessary colors for tomorrow night fish theme

---
+++ fish/fish_theme.fish
@@ -0,0 +1,27 @@
+############################################################################
+# Tomorrow Night theme
+# Take the result of => set -U | grep fish_color_
+# Write them all down here
+# Inspired by: https://superuser.com/a/1243748/502396
+############################################################################
+set --global --export fish_color_autosuggestion '556'  'brblack'
+set --global --export fish_color_cancel -r
+set --global --export fish_color_command c397d8
+set --global --export fish_color_comment 990000
+set --global --export fish_color_cwd green
+set --global --export fish_color_cwd_root red
+set --global --export fish_color_end c397d8
+set --global --export fish_color_error ff0000
+set --global --export fish_color_escape 00a6b2
+set --global --export fish_color_history_current --bold
+set --global --export fish_color_host normal
+set --global --export fish_color_match --background=brblue
+set --global --export fish_color_normal normal
+set --global --export fish_color_operator 00a6b2
+set --global --export fish_color_param 00afff
+set --global --export fish_color_quote b9ca4a
+set --global --export fish_color_redirection 70c0b1
+set --global --export fish_color_search_match 'bryellow'  '--background=brblack'
+set --global --export fish_color_selection 'white'  '--bold'  '--background=brblack'
+set --global --export fish_color_user brgreen
+set --global --export fish_color_valid_path --underlineet --global --export
