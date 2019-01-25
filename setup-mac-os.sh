2eb71fbca5d8b3d4925c9c924b40fb0d8dc89d37
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-25 05:35:22 +0200

    update setup-mac-os.sh

--- setup-mac-os.sh
+++ setup-mac-os.sh
@@ -5,8 +5,7 @@ main() {
     ask_for_sudo
     # Installing Homebrew, the basis of anything and everything
     install_homebrew
-    # Cloning Dotfiles repository for install_packages_with_brewfile
-    # to have access to Brewfile
+    # Cloning Dotfiles repository for install_packages_with_brewfile to have access to Brewfile
     clone_dotfiles_repo
     # Installing all packages in Dotfiles repository's Brewfile
     install_packages_with_brewfile
