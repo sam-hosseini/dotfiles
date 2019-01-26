1a2834d49dbf8af23a1d5e23d665f0ea961187bc
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-26 11:48:07 +0200

    update tt fish shell function to kill all bg jobs

--- fish/functions/tt.fish
+++ fish/functions/tt.fish
@@ -1,3 +1,8 @@
 function tt
-    caffeinate -d &; tmuxp load ~/personal/dotfiles/tmux/tmux_startup_windows.yaml
+    # Kill all background jobs
+    jobs -p | xargs kill
+    # Send signals to macOS to prevent sleep
+    caffeinate -d &
+    # Load tmux session using configuration file
+    tmuxp load ~/personal/dotfiles/tmux/tmux_startup_windows.yaml
 end
