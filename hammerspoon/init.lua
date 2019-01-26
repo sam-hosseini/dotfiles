5fcddcb077c688c260e8d28ad8ada77a392dc348
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-26 17:45:15 +0200

    add support for complete window management using MiroWindowsManager spoon in hammerspoon, fully replacing Spectacle

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,27 @@
 ----------------------------------------------------
+-- Spoons
+----------------------------------------------------
+hs.loadSpoon("MiroWindowsManager")
+
+----------------------------------------------------
+-- Variables
+----------------------------------------------------
+local HYPER_KEY = {'cmd', 'alt', 'ctrl', 'shift'}
+
+
+----------------------------------------------------
+-- Window management
+----------------------------------------------------
+hs.window.animationDuration = 0.2
+spoon.MiroWindowsManager:bindHotkeys({
+  left = {HYPER_KEY, "left"},
+  down = {HYPER_KEY, "down"},
+  up = {HYPER_KEY, "up"},
+  right = {HYPER_KEY, "right"},
+  fullscreen = {HYPER_KEY, "m"}
+})
+
+----------------------------------------------------
 -- Automatic configuration reloader
 ----------------------------------------------------
 function reloadConfig(files)
