b417086e85f9d74bd5d4f7dbfe79ba176c9e6093
Sam Hosseini <non-existent-email@hosseini.io>
2019-01-27 19:23:06 +0200

    use a spoon instead of manually writing a configuration reloader in hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,6 +2,7 @@
 -- Spoons
 ----------------------------------------------------
 hs.loadSpoon("MiroWindowsManager")
+hs.loadSpoon("ReloadConfiguration")
 
 ----------------------------------------------------
 -- Variables
@@ -25,17 +26,5 @@ spoon.MiroWindowsManager:bindHotkeys({
 ----------------------------------------------------
 -- Automatic configuration reloader
 ----------------------------------------------------
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-
-myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+spoon.ReloadConfiguration:start()
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
