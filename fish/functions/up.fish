bf057422b2d1ca7d915a73b9f2b5551947244324
Sam Hosseini <non-existent-email@hosseini.io>
2019-02-02 13:18:04 +0200

    add proper sections to up fish shell function

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,7 +1,13 @@
 function up
+    echo -e '####################################\n# Software Update \n####################################'
     sudo softwareupdate --install --all
+    echo -e '####################################\n# Brew \n####################################'
     brew update; brew upgrade; brew cask upgrade; brew cleanup; brew doctor;
+    echo -e '####################################\n# Pip \n####################################'
     pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests tmuxp virtualenv
+    echo -e '####################################\n# Yarn \n####################################'
     yarn global upgrade --silent
+    echo -e '####################################\n# Oh-My-Fish \n####################################'
     omf install; omf update;
+    echo -e '####################################\n# Done \n####################################'
 end
