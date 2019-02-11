62a8346484086e21c5d3d64a05b9357ada25a023
Sam Hosseini <non-existent-email@hosseini.io>
2019-02-11 15:57:02 +0200

    add extract.fish fish shell function taken from @dideler Dotfiles repo

---
+++ fish/functions/extract.fish
@@ -0,0 +1,49 @@
+# Taken from @dideler Dotfiles repo. Thank you so much Dideler!
+
+function extract --description "Expand or extract bundled & compressed files"
+
+  for file in $argv
+
+    if test -f $file
+
+      echo -s "Extracting " (set_color --bold blue) $file (set_color normal)
+
+      switch $file
+
+        case "*.tar"
+          tar -xvf $file
+
+        case "*.tar.bz2" "*.tbz2"
+          tar --bzip2 -xvf $file
+
+        case "*.tar.gz" "*.tgz"
+          tar --gzip -xvf $file
+
+        case "*.bz" "*.bz2"
+          bunzip2 $file
+
+        case "*.gz"
+          gunzip $file
+
+        case "*.rar"
+          unrar x $file
+
+        case "*.zip"
+          unzip -uo $file -d (basename $file .zip)
+
+        case "*.Z"
+          uncompress $file
+
+        case "*.pax"
+          pax -r < $file
+
+        case '*'
+          echo "Extension not recognized, cannot extract $file"
+
+      end
+
+    else
+      echo "$file is not a valid file"
+    end
+  end
+end
