f6cfec7b5e3deba60ad89b08062b346187419dd4
Sam Hosseini <non-existent-email@hosseini.io>
2019-06-05 23:47:01 +0300

    update up function in fish shell to include upgrading all the casks except those which don't have versioning

--- fish/functions/up.fish
+++ fish/functions/up.fish
@@ -1,13 +1,22 @@
 function up
     echo -e '####################################\n# Software Update \n####################################'
     sudo softwareupdate --install --all
+
     echo -e '####################################\n# Brew \n####################################'
-    brew update; brew upgrade; brew cask upgrade; brew cleanup; brew doctor;
+    brew update
+    brew upgrade
+    brew cask outdated --greedy --verbose | ack --invert-match latest | awk '{print $1;}' | xargs brew cask upgrade
+    brew cleanup
+    brew doctor
+
     echo -e '####################################\n# Pip \n####################################'
     pip3 install --upgrade --quiet pip setuptools wheel powerline-status requests tmuxp virtualenv django mypy
+
     echo -e '####################################\n# Yarn \n####################################'
     yarn global upgrade --silent
+
     echo -e '\n####################################\n# Oh-My-Fish \n####################################'
     omf install; omf update;
+
     echo -e '####################################\n# Done \n####################################'
 end
