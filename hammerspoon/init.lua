65475584b7042ea1a67174a5481c990a12999568
Sam Hosseini <non-existent-email@hosseini.io>
2019-09-24 14:51:07 +0300

    move reload_configuration to the end of the list of items to load in hammerspoon modules

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,9 @@
 local modules_to_load = {
     'hammerspoon_preferences',
     'iterm_startup',
-    'reload_configuration',
     'window_management',
     'connect_to_wifi',
+    'reload_configuration', --> last item to ensure everything loaded nicely
 }
 
 for _, module in pairs(modules_to_load) do
