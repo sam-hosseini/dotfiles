0b75e314734edfdbc2b7fe2820ee0aa61bc26819
Sam Hosseini <non-existent-email@hosseini.io>
2019-09-24 14:14:49 +0300

    increase the delay before clicking on connecting to wifi

--- hammerspoon/connect_to_wifi.lua
+++ hammerspoon/connect_to_wifi.lua
@@ -2,7 +2,7 @@ hs.osascript.applescript([[
 tell application "System Events" to tell process "SystemUIServer"
 	tell menu bar item 5 of menu bar 1
         click
-        delay 3
+        delay 5
         click menu item "King in the North" of menu 1
 	end tell
 end tell
