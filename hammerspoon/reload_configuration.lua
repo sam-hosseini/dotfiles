1a084e084a1978996a5d14fb9e5e1fab657a5c24
Sam Hosseini <non-existent-email@hosseini.io>
2019-10-07 10:03:29 +0300

    remove notification_config from hammerspoon/reload_configuration.lua as that's not the place to show that all configs has been loaded completely

--- hammerspoon/reload_configuration.lua
+++ hammerspoon/reload_configuration.lua
@@ -1,11 +1,3 @@
 hs.loadSpoon("ReloadConfiguration")
 
 spoon.ReloadConfiguration:start()
-
-local notification_config = {
-    title="Hammerspoon",
-    informativeText="Config loaded",
-    withdrawAfter=1.25
-}
-
-hs.notify.new(notification_config):send()
