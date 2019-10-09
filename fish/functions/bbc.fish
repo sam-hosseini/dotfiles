8b0f0d6f9aff260d16630c8b551b90f3a5ec1d9c
Sam Hosseini <non-existent-email@hosseini.io>
2019-10-09 16:42:36 +0300

    update bbc fish shell function to combine all Brewfiles to be able to function correctly

--- fish/functions/bbc.fish
+++ fish/functions/bbc.fish
@@ -1,3 +1,10 @@
 function bbc
-    brew bundle cleanup --file=~/personal/dotfiles/brew/macOS.Brewfile --force
+    set --local TAP  ~/personal/dotfiles/brew/Brewfile_tap
+    set --local BREW ~/personal/dotfiles/brew/Brewfile_brew
+    set --local CASK ~/personal/dotfiles/brew/Brewfile_cask
+    set --local MAS  ~/personal/dotfiles/brew/Brewfile_mas
+    set --local BREWFILE ~/personal/dotfiles/brew/Brewfile
+
+    cat $TAP $BREW $CASK $MAS > $BREWFILE
+    brew bundle cleanup --file=$BREWFILE --force
 end
