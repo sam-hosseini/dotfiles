8716e1c4e84cc8250bb1e14e081138eff71f4100
Sam Hosseini <non-existent-email@hosseini.io>
2019-10-09 13:18:09 +0300

    remove connect_to_wifi hammerspoon module as in macOS Catalina, a popup shows to join personal hotspot

--- hammerspoon/connect_to_wifi.lua
+++
@@ -1,9 +0,0 @@
-hs.osascript.applescript([[
-tell application "System Events" to tell process "SystemUIServer"
-	tell menu bar item 5 of menu bar 1
-        click
-        delay 5
-        click menu item "King in the North" of menu 1
-	end tell
-end tell
-]])
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,7 +3,6 @@ local modules_to_load = {
     'hammerspoon_preferences',
     'iterm_startup',
     'window_management',
-    'connect_to_wifi',
 }
 
 for _, module in pairs(modules_to_load) do
