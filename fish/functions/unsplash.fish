4fbe262721131e7c73683ecfa262b572a0e2af38
Sam Hosseini <non-existent-email@hosseini.io>
2019-10-09 21:33:25 +0300

    update unsplash fish shell function not to put any image in dotfiles repo

--- fish/functions/unsplash.fish
+++ fish/functions/unsplash.fish
@@ -3,10 +3,7 @@ function unsplash
     set --local PHOTO_ID $argv[1]
     set --local RANDOM_NUMBER (random)
 
-    set --local WALLPAPER_IMAGES ~/personal/dotfiles/macOS/todays_picture_*.jpg
     set --local DROPBOX_IMAGES ~/Dropbox/Uploads/todays_picture_*.jpg
-
-    set --local WALLPAPER ~/personal/dotfiles/macOS/todays_picture_$RANDOM_NUMBER.jpg
     set --local DROPBOX_WALLPAPER ~/Dropbox/Uploads/todays_picture_$RANDOM_NUMBER.jpg
 
     set --local BASE_URL "https://api.unsplash.com"
@@ -14,17 +11,11 @@ function unsplash
     set --local UNSPLASH_RESPONSE (http --body $API_IMAGE_URL "Authorization: Client-ID $UNSPLASH_ACCESS_KEY")
     set --local IMAGE_URL (echo $UNSPLASH_RESPONSE | jq --raw-output ".urls.full")
 
-    ############################################################################
-    # Downloading a random picture and setting it as wallpaper
-    ############################################################################
-    rm -f $WALLPAPER_IMAGES
-    wget --quiet --output-document=$WALLPAPER $IMAGE_URL
-    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$WALLPAPER\""
-
-    ############################################################################
-    # Copying picture to Dropbox so that IFTTT can download it to iOS pictures
-    ############################################################################
+    ##################################################################################################
+    # Downloading to Dropbox so that IFTTT can download it to iOS pictures and setting it as wallpaper
+    ##################################################################################################
     rm -f $DROPBOX_IMAGES
-    cp $WALLPAPER $DROPBOX_WALLPAPER
+    wget --quiet --output-document=$DROPBOX_WALLPAPER $IMAGE_URL
+    osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \"$DROPBOX_WALLPAPER\""
 
 end
