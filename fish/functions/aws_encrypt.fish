3efb39fe52280251db4a86b24538d5852084ecb2
Sam Hosseini <non-existent-email@hosseini.io>
2019-10-19 22:32:52 +0300

    update aws_encrypt fish shell function to get AWS_CMK_ALIAS from 1password cli application instead of env var

--- fish/functions/aws_encrypt.fish
+++ fish/functions/aws_encrypt.fish
@@ -1,4 +1,7 @@
 function aws_encrypt
+    1password_session
+    set --local AWS_CMK_ALIAS (op get item 'AWS sam.hosseini' | jq --raw-output '.details.sections[1].fields[1].v')
+
     set --local STRING_TO_ENCRYPT $argv[1]
 
     aws kms encrypt --plaintext $STRING_TO_ENCRYPT --key-id $AWS_CMK_ALIAS --output text --query CiphertextBlob | pbcopy
