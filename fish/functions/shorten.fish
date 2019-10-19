55bec0305daaef806b9f89dbd7f8061cc1238560
Sam Hosseini <non-existent-email@hosseini.io>
2019-10-19 22:28:10 +0300

    update shorten fish shell function to get BITLY_API_TOKEN from 1password cli application instead of env var

--- fish/functions/shorten.fish
+++ fish/functions/shorten.fish
@@ -1,4 +1,7 @@
 function shorten
+    1password_session
+    set --local BITLY_API_TOKEN (op get item bitly | jq --raw-output '.details.sections[1].fields[0].v')
+
     set --local API_URL https://api-ssl.bitly.com/v4/shorten
     set --local HEADER "Authorization:Bearer $BITLY_API_TOKEN"
     set --local LONG_URL $argv[1]
