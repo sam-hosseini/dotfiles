d466b3637a1ef87435e604a4656559dc9c55617a
Sam Hosseini <git-email@hosseini.io>
2019-10-27 13:17:22 +0200

    update personal_hotspot fish shell function to ask for password on commandline as without an internet connection, 1password_session cannot function anyway

--- fish/functions/personal_hotspot.fish
+++ fish/functions/personal_hotspot.fish
@@ -1,6 +1,5 @@
 function personal_hotspot
-    1password_session
-    set --local PERSONAL_HOTSPOT_PASSWORD (op get item 'Personal Hotspot' | jq --raw-output '.details.sections[1].fields[0].v')
+    set --local PERSONAL_HOTSPOT_PASSWORD $argv[1]
 
     networksetup -setairportnetwork en0 "King in the North" $PERSONAL_HOTSPOT_PASSWORD
 end
