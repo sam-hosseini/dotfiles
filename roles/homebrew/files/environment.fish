bc0f75a83efeac20e5b1ab58c375637cfd0faffd
Sam Hosseini <git-email@hosseini.io>
2022-01-03 00:36:11 +0100

    add HOMEBREW_NO_INSTALL_CLEANUP to homebrew fish env file

--- roles/homebrew/files/environment.fish
+++ roles/homebrew/files/environment.fish
@@ -1,2 +1,3 @@
 set --global --export HOMEBREW_CASK_OPTS            "--no-quarantine"
 set --global --export HOMEBREW_NO_AUTO_UPDATE       true
+set --global --export HOMEBREW_NO_INSTALL_CLEANUP   true
