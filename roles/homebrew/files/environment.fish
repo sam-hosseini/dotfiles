5c24832d9403738c972e4411f102b39915b735cb
Sam Hosseini <git-email@hosseini.io>
2022-02-02 01:10:23 +0300

    add HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK to homebrew fish env file

--- roles/homebrew/files/environment.fish
+++ roles/homebrew/files/environment.fish
@@ -1,3 +1,4 @@
-set --global --export HOMEBREW_CASK_OPTS            "--no-quarantine"
-set --global --export HOMEBREW_NO_AUTO_UPDATE       true
-set --global --export HOMEBREW_NO_INSTALL_CLEANUP   true
+set --global --export HOMEBREW_CASK_OPTS                        "--no-quarantine"
+set --global --export HOMEBREW_NO_AUTO_UPDATE                   true
+set --global --export HOMEBREW_NO_INSTALL_CLEANUP               true
+set --global --export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK    true
