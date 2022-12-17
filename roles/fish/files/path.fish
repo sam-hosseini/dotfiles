bfaace93d4238cff0b95645cf928ca2efe880863
Sam Hosseini <git-email@hosseini.io>
2022-12-17 15:43:01 +0100

    add latest python symlinks to fish PATH

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -1,3 +1,5 @@
+set --local --append OWN_PATH /usr/local/opt/python@3.11/libexec/bin
+
 set --local --append OWN_PATH /usr/local/bin      # Where brew symlinks most packages
 set --local --append OWN_PATH /usr/local/sbin     # Where brew symlinks some packages
 set --local --append OWN_PATH ~/.local/bin        # Where pipx symlinks its  packages
