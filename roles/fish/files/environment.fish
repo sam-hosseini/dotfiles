edb964c0fb6cfced0a4731fa197d98fc2f6255ac
Sam Hosseini <git-email@hosseini.io>
2023-09-19 13:33:37 +0200

    disable tldr auto update

--- roles/fish/files/environment.fish
+++ roles/fish/files/environment.fish
@@ -4,3 +4,4 @@ set --global --export fish_greeting             ''
 set --global --export LC_ALL                    en_US.UTF-8 # Set locale https://bit.ly/3mNj6Ia
 set --global --export OP_FORMAT                 json
 set --global --export PYTHONDONTWRITEBYTECODE   true
+set --global --export TLDR_AUTO_UPDATE_DISABLED true
