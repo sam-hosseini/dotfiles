9d5a1d5224f104381c0d19332e17a9b3e9bc4b62
Sam Hosseini <git-email@hosseini.io>
2023-10-27 18:06:41 +0200

    update python@3.11 to python@3.12

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -1,4 +1,4 @@
-set --local --append OWN_PATH /usr/local/opt/python@3.11/libexec/bin
+set --local --append OWN_PATH /usr/local/opt/python@3.12/libexec/bin
 
 set --local --append OWN_PATH /usr/local/bin      # Where brew symlinks most packages
 set --local --append OWN_PATH /usr/local/sbin     # Where brew symlinks some packages
