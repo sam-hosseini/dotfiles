fa31fcfe6db476398fcd391ef11e1004d287abb2
Sam Hosseini <git-email@hosseini.io>
2023-11-28 16:22:52 +0100

    sort lines of environment.fish

--- roles/fish/files/environment.fish
+++ roles/fish/files/environment.fish
@@ -2,8 +2,8 @@ set --global --export BAT_THEME                 Dracula
 set --global --export fish_emoji_width          2
 set --global --export fish_greeting             ''
 set --global --export LC_ALL                    en_US.UTF-8 # Set locale https://bit.ly/3mNj6Ia
+set --global --export NPM_CONFIG_AUDIT          false
+set --global --export NPM_CONFIG_FUND           false
 set --global --export OP_FORMAT                 json
 set --global --export PYTHONDONTWRITEBYTECODE   true
 set --global --export TLDR_AUTO_UPDATE_DISABLED true
-set --global --export NPM_CONFIG_FUND           false
-set --global --export NPM_CONFIG_AUDIT          false
