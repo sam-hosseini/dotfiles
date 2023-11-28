3cf306f00a4511525689387f3d6f62f29376965c
Sam Hosseini <git-email@hosseini.io>
2023-11-28 16:36:35 +0100

    update locale to en_GB
    https://stackoverflow.com/a/7246630/5208999

--- roles/fish/files/environment.fish
+++ roles/fish/files/environment.fish
@@ -1,7 +1,7 @@
 set --global --export BAT_THEME Dracula
 set --global --export fish_emoji_width 2
 set --global --export fish_greeting ''
-set --global --export LC_ALL en_US.UTF-8 # Set locale https://bit.ly/3mNj6Ia
+set --global --export LC_ALL en_GB.UTF-8
 set --global --export NPM_CONFIG_AUDIT false
 set --global --export NPM_CONFIG_FUND false
 set --global --export OP_FORMAT json
