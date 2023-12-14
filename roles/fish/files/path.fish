cd3bca0f2dd5a7c4f6e2f3ea744d485d35f52fd8
Sam Hosseini <git-email@hosseini.io>
2023-12-14 22:23:29 +0100

    add postgresql@16 to homebrew_formulae

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -1,4 +1,5 @@
 set --local --append OWN_PATH /usr/local/opt/node@20/bin
+set --local --append OWN_PATH /usr/local/opt/postgresql@16/bin
 set --local --append OWN_PATH /usr/local/opt/python@3.12/libexec/bin
 
 set --local --append OWN_PATH /usr/local/bin      # Where brew symlinks most packages
