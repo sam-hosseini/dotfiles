ee32d61b61c63198947cc54d0e46e12b298cbce7
Sam Hosseini <git-email@hosseini.io>
2023-12-24 12:34:03 +0100

    remove find fish function

--- roles/fish/files/functions/find.fish
+++
@@ -1,3 +0,0 @@
-function find --wraps "fd"
-    fd $argv
-end
--- roles/fish/files/functions/pull_repos.fish
+++ roles/fish/files/functions/pull_repos.fish
@@ -1,5 +1,5 @@
 function pull_repos
-    set --local REPOS (find --type directory --maxdepth 1 . ~/repos/)
+    set --local REPOS (fd --type directory --maxdepth 1 . ~/repos/)
 
     parallel --linebuffer -j (count $REPOS) git -C {} pull --quiet ::: $REPOS
 end
