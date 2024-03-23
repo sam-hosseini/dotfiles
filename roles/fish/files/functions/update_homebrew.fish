196eaa7398afe86ac073dd3945fc5f620b97aab8
Sam Hosseini <git-email@hosseini.io>
2024-03-23 15:43:17 +0300

    fix «brew update» command noisy output in update_homebrew

--- roles/fish/files/functions/update_homebrew.fish
+++ roles/fish/files/functions/update_homebrew.fish
@@ -1,5 +1,5 @@
 function update_homebrew
-    brew update | grep --invert-match "Already up-to-date."
+    brew update 2>/dev/null | grep --invert-match "Already up-to-date."
 
     brew upgrade --formulae
 
