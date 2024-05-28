bf68aaf54ca0621fc4bbf85c50dc907348ff702b
Sam Hosseini <git-email@hosseini.io>
2024-05-28 12:29:15 +0200

    add ruby and gems symlinks to fish PATH

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -2,6 +2,9 @@ set --local --append OWN_PATH /usr/local/opt/node@20/bin
 set --local --append OWN_PATH /usr/local/opt/postgresql@16/bin
 set --local --append OWN_PATH /usr/local/opt/python@3.12/libexec/bin
 
+set --local --append OWN_PATH /usr/local/opt/ruby/bin
+set --local --append OWN_PATH /usr/local/lib/ruby/gems/3.3.0/bin
+
 set --local --append OWN_PATH /usr/local/bin      # Where brew symlinks most packages
 set --local --append OWN_PATH /usr/local/sbin     # Where brew symlinks some packages
 set --local --append OWN_PATH ~/.local/bin        # Where pipx symlinks its  packages
