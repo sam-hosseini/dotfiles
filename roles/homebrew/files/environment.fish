afc1f70fb23af7b185a1c7cb78e59fcef1c9a7d2
Sam Hosseini <git-email@hosseini.io>
2024-08-25 21:31:07 +0200

    add HOMEBREW_NO_ANALYTICS to homebrew fish env file

--- roles/homebrew/files/environment.fish
+++ roles/homebrew/files/environment.fish
@@ -1,4 +1,5 @@
 set --global --export HOMEBREW_CASK_OPTS                        "--no-quarantine"
+set --global --export HOMEBREW_NO_ANALYTICS                     true
 set --global --export HOMEBREW_NO_AUTO_UPDATE                   true
 set --global --export HOMEBREW_NO_INSTALL_CLEANUP               true
 set --global --export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK    true
