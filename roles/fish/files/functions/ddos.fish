012bdf9221ee2712e630a961084282f2abad772e
Sam Hosseini <git-email@hosseini.io>
2024-08-26 10:40:11 +0200

    replace ddosify with ali

--- roles/fish/files/functions/ddos.fish
+++ roles/fish/files/functions/ddos.fish
@@ -1,5 +1,9 @@
 function ddos
     set --local TARGET $argv[1]
 
-    ddosify -m GET -T 5 -l linear -n 500 -d 10 -t $TARGET
+    ali $TARGET \
+        --method GET \
+        --timeout 5s \
+        --duration 10s \
+        --rate 50
 end
