6151fe2773c3f7c01153369efa5fc808bec2fbc6
Sam Hosseini <git-email@hosseini.io>
2025-01-05 15:18:22 +0100

    update gems symlink to 3.4.0

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -3,7 +3,7 @@ set --local --append OWN_PATH /usr/local/opt/postgresql@16/bin
 set --local --append OWN_PATH /usr/local/opt/python@3.12/libexec/bin
 
 set --local --append OWN_PATH /usr/local/opt/ruby/bin
-set --local --append OWN_PATH /usr/local/lib/ruby/gems/3.3.0/bin
+set --local --append OWN_PATH /usr/local/lib/ruby/gems/3.4.0/bin
 
 set --local --append OWN_PATH /usr/local/bin      # Where brew symlinks most packages
 set --local --append OWN_PATH /usr/local/sbin     # Where brew symlinks some packages
