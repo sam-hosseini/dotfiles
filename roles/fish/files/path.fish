5c034b3454c60f8e8a26b9a018ff5263e704e164
Sam Hosseini <git-email@hosseini.io>
2025-10-03 07:52:25 +0300

    refactor fish role to use brew prefix

--- roles/fish/files/path.fish
+++ roles/fish/files/path.fish
@@ -1,12 +1,14 @@
-set --local --append OWN_PATH /usr/local/opt/node@22/bin
-set --local --append OWN_PATH /usr/local/opt/postgresql@16/bin
-set --local --append OWN_PATH /usr/local/opt/python@3.12/libexec/bin
+set --local BREW_PREFIX (brew --prefix)
 
-set --local --append OWN_PATH /usr/local/opt/ruby/bin
-set --local --append OWN_PATH /usr/local/lib/ruby/gems/3.4.0/bin
+set --local --append OWN_PATH $BREW_PREFIX/opt/node@22/bin
+set --local --append OWN_PATH $BREW_PREFIX/opt/postgresql@16/bin
+set --local --append OWN_PATH $BREW_PREFIX/opt/python@3.12/libexec/bin
 
-set --local --append OWN_PATH /usr/local/bin      # Where brew symlinks most packages
-set --local --append OWN_PATH /usr/local/sbin     # Where brew symlinks some packages
-set --local --append OWN_PATH ~/.local/bin        # Where pipx symlinks its  packages
+set --local --append OWN_PATH $BREW_PREFIX/opt/ruby/bin
+set --local --append OWN_PATH $BREW_PREFIX/lib/ruby/gems/3.4.0/bin
+
+set --local --append OWN_PATH $BREW_PREFIX/bin      # Where brew symlinks most packages
+set --local --append OWN_PATH $BREW_PREFIX/sbin     # Where brew symlinks some packages
+set --local --append OWN_PATH ~/.local/bin          # Where pipx symlinks its  packages
 
 set --global --export fish_user_paths $OWN_PATH
