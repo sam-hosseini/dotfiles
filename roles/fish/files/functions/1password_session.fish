daf8c674d3801f4fc6d520ad06c5db41350015b0
Sam Hosseini <git-email@hosseini.io>
2026-01-19 14:39:39 +0200

    update 1password_session fish function for 1Password CLI 2.32

--- roles/fish/files/functions/1password_session.fish
+++ roles/fish/files/functions/1password_session.fish
@@ -1,7 +1,3 @@
 function 1password_session
-    set --local USER_UUID (op account list | jq --raw-output '.[0].user_uuid')
-
-    while not set --query OP_SESSION_$USER_UUID
-        eval $(op signin)
-    end
+    op signin
 end
