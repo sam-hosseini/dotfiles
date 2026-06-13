b09fe4431dcb61d1f51dbb1b5fe611a68657be77
Sam Hosseini <git-email@hosseini.io>
2026-06-13 13:14:45 +0200

    replace obsolete no-quarantine flag with an ansible task for homebrew

--- roles/homebrew/files/environment.fish
+++ roles/homebrew/files/environment.fish
@@ -1,4 +1,3 @@
-set --global --export HOMEBREW_CASK_OPTS                        "--no-quarantine"
 set --global --export HOMEBREW_NO_ANALYTICS                     true
 set --global --export HOMEBREW_NO_AUTO_UPDATE                   true
 set --global --export HOMEBREW_NO_INSTALL_CLEANUP               true
