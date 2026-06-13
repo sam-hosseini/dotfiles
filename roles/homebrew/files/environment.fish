1b476db0deb79af3a197de33f64c29b596f221c5
Sam Hosseini <git-email@hosseini.io>
2026-06-13 13:19:46 +0200

    disable homebrew's ask mode

--- roles/homebrew/files/environment.fish
+++ roles/homebrew/files/environment.fish
@@ -1,3 +1,4 @@
+set --global --export HOMEBREW_NO_ASK                           true
 set --global --export HOMEBREW_NO_ANALYTICS                     true
 set --global --export HOMEBREW_NO_AUTO_UPDATE                   true
 set --global --export HOMEBREW_NO_INSTALL_CLEANUP               true
